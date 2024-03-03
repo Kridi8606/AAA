c17e3a6ad72711e124d0782030b4127839a9de48
Kridi8606 <www.fedakridi.ru@gmail.com>
2024-03-03 13:48:25 +0300

    Задание четвёртой недели

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Networking.Match;
+﻿using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
